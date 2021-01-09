b0b911ea6f138fbfb61f459a38aa81876ade0028
Антонина Максимова <tonkastar@mail.ru>
2021-01-10 02:03:35 +0300

    no message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,11 +10,11 @@ const App = () => {
 	return (<Router className={styles.color}>
 				<div className={styles.wrap}>
 					<MenuList className={styles.menu}>
-			          <Link to ='/about' className={styles.link}><MenuItem className={styles.menuItem} >Обо мне</MenuItem></Link>
+			          <Link to ='/reactAppTodo' className={styles.link}><MenuItem className={styles.menuItem} >Обо мне</MenuItem></Link>
 			          <Link to ='/todo'className={styles.link}><MenuItem className={styles.menuItem} >Задачи</MenuItem></Link>
 			        </MenuList>
 					<div className={styles.inner}>
-						<Route path ='/about' component={About}/>
+						<Route path ='/reactAppTodo' component={About}/>
 						<Route path ='/todo' component={Todo}/>
 					</div>
 				</div>
